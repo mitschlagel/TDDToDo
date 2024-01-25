d067471ab6b33cc1a416d5d86787a78427ace999
Spencer Jones <sjonesdrums@gmail.com>
2024-01-24 22:40:54 -0600

    Add details vc

---
+++ TDDToDoTests/ToDoItemDetailsViewControllerTests.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoItemDetailsViewControllerTests.swift
+//  TDDToDoTests
+//
+//  Created by Spencer Jones on 1/24/24.
+//
+
+import XCTest
+@testable import TDDToDo
+
+final class ToDoItemDetailsViewControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        var sut: ToDoItemDetailsViewControllerTests!
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
